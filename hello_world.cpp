397660deb5f2762345c42ea9aff4f4d46883473e
Vector05 <mathurraghav23@gmail.com>
2026-06-16 12:25:37 +0530

    updated congif

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // The standard Input/Output Stream library
 
 int main() {
-  cout << "Hello World!";
-  return 0;
+    // cout uses the stream insertion operator (<<) to push text to the console
+    std::cout << "Hello from C++! This works flawlessly everywhere." << std::endl;
+    
+    // You can chain multiple things together easily
+    int version = 16;
+    std::cout << "My compiler version is: " << version << std::endl;
+    
+    return 0;
 }
